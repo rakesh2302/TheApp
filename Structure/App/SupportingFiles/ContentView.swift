453459670610c930417098e74e7b1ba3af779693
Rakeshshetty0023 <50423484+Rakeshshetty0023@users.noreply.github.com>
2022-12-19 09:47:12 -0800

    Break rules

--- Structure/App/SupportingFiles/ContentView.swift
+++ Structure/App/SupportingFiles/ContentView.swift
@@ -24,6 +24,7 @@ struct ContentView: View {
             if self.scaleImage {
                 let _ = print("")
                 
+                
             } else {
                 let _ = print("")
             }
